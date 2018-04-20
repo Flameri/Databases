78ec48ff25a33413ac0ebc64117cb3f2ab15a4a1
Flameri <arto.lindgren1@gmail.com>
2018-04-20 13:03:50 +0300

    Added functions

--- BankDB/BankDB/Program.cs
+++ BankDB/BankDB/Program.cs
@@ -12,47 +12,76 @@ namespace BankDB
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            BankRepository bankRepository = new BankRepository();
-            //Bank b = new Bank("Kontulan pankki", "KDAFIHH");
-            //Bank b2 = new Bank("Sammonlahden pankki", "SDAFIHH");
-            //Bank b3 = new Bank("Danske Bank", "DANSKFI");
-            //bankRepository.Create(b2);
-            //bankRepository.Create(b);
-            //bankRepository.Create(b3);
-            //bankRepository.Delete(4);
-
-            //Bank updateBank = bankRepository.GetBankById(2);
-            //updateBank.Name = "Roope Ankan Pankki";
-            //updateBank.Bic = "RAPFI";
-            //bankRepository.Update(updateBank);
-
+            Print();
+            
+            Console.WriteLine("\n<Enter> lopettaa ohjelman suorituksen!");
+            Console.ReadLine();
+        }
+        static void CreateCustomer()
+        {
             CustomerRepository customerRepository = new CustomerRepository();
-            //Customer c1 = new Customer("Lucky", "Luke");
-            //Customer c2 = new Customer("Musta", "Kaapu");
-            //customerRepository.CreateCustomer(c1);
-            //customerRepository.CreateCustomer(c2);
-            //customerRepository.DeleteCustomer(5);
-
+            Customer c1 = new Customer("Lucky", "Luke");
+            Customer c2 = new Customer("Musta", "Kaapu");
+            customerRepository.CreateCustomer(c1);
+            customerRepository.CreateCustomer(c2);   
+        }
+        static void UpdateCustomer()
+        {
+            CustomerRepository customerRepository = new CustomerRepository();
             //ei voi muuttaa long to int?
-            //Customer updateCustomer = customerRepository.GetCustomerById(3);
-            //updateCustomer.FirstName = "Fred";
-            //updateCustomer.LastName = "Flintstone";
-            //customerRepository.UpdateCustomer(updateCustomer, 3);
-
+            Customer updateCustomer = customerRepository.GetCustomerById(3);
+            updateCustomer.FirstName = "Fred";
+            updateCustomer.LastName = "Flintstone";
+            customerRepository.UpdateCustomer(updateCustomer, 3);
+        }
+        static void DeleteCustomer()
+        {
+            CustomerRepository customerRepository = new CustomerRepository();
+            customerRepository.DeleteCustomer(5);
+        }
+        static void CreateBank()
+        {
+            BankRepository bankRepository = new BankRepository();
+            Bank b = new Bank("Kontulan pankki", "KDAFIHH");
+            Bank b2 = new Bank("Sammonlahden pankki", "SDAFIHH");
+            Bank b3 = new Bank("Danske Bank", "DANSKFI");
+            bankRepository.Create(b2);
+            bankRepository.Create(b);
+            bankRepository.Create(b3);            
+        }
+        static void UpdateBank()
+        {
+            BankRepository bankRepository = new BankRepository();
+            Bank updateBank = bankRepository.GetBankById(2);
+            updateBank.Name = "Roope Ankan Pankki";
+            updateBank.Bic = "RAPFI";
+            bankRepository.Update(updateBank);
+        }
+        static void DeleteBank()
+        {
+            BankRepository bankRepository = new BankRepository();
+            bankRepository.Delete(4);
+        }
+        static void CreateTransaction()
+        {
             AccountRepository accountRepository = new AccountRepository();
 
             TransactionRepository transactionRepository = new TransactionRepository();
-            //Transaction transaction = new Transaction
-            //{
-            //   Iban = "FI49123456789",
-            //   Amount = -3000,
-            //   TimeStamp = DateTime.Today
-            //};
-            //accountRepository.CreateTransaction(transaction);
+            Transaction transaction = new Transaction
+            {
+                Iban = "FI49123456789",
+                Amount = -3000,
+                TimeStamp = DateTime.Today
+            };
+            accountRepository.CreateTransaction(transaction);
+        }
+        static void Print()
+        {
+            BankRepository bankRepository = new BankRepository();
 
             var bankCustomers = bankRepository.GetTransactionsFromBankCustomersAccounts();
 
-            foreach (var bC in bankCustomers) 
+            foreach (var bC in bankCustomers)
             {
                 Console.WriteLine(bC.ToString());
                 foreach (var c in bC.Customer)
@@ -68,9 +97,6 @@ namespace BankDB
                     }
                 }
             }
-
-            Console.WriteLine("\n<Enter> lopettaa ohjelman suorituksen!");
-            Console.ReadLine();
         }
     }
 }
